417ec2c2cf8632d439fe33f4ef6eb7ed5f73b92a
Allen Roberts <allenrobertsdev@gmail.com>
2026-08-19 17:21:38 -0400

    Fixed small typing issue

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -10,7 +10,7 @@ interface Props {
         email: string;
         bio: string | null;
         profile_image: string | null;
-        profile_url: string | null;
+        profile_url: string | undefined;
     };
     recipes: Recipe[];
 }
